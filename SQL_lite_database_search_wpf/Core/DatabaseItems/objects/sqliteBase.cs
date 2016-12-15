680ab473cc4ad086feb145516c731dc351e65580
Henri Carnot <henri.carnot@hotmail.fr>
2016-12-15 20:51:15 +0100

    sqliteElements

--- SQL_lite_database_search_wpf/Core/DatabaseItems/objects/sqliteBase.cs
+++ SQL_lite_database_search_wpf/Core/DatabaseItems/objects/sqliteBase.cs
@@ -9,6 +9,7 @@ namespace SQL_lite_database_search_wpf.Core.DatabaseItems.objects
     public class sqliteBase
     {
         public string valueName { get; set; }
+        public object value { get; set; }
         public sqliteBase(string Name) { valueName = Name; }
     }
 }
--- SQL_lite_database_search_wpf/Core/DatabaseItems/objects/sqliteObjects.cs
+++ SQL_lite_database_search_wpf/Core/DatabaseItems/objects/sqliteObjects.cs
@@ -9,21 +9,21 @@ namespace SQL_lite_database_search_wpf.Core.DatabaseItems.objects
     public class sqliteBool : sqliteBase
     {
         public sqliteBool(string Name) : base(Name) { }
-        public bool value { get; set; }
+        public new bool value { get; set; }
     }
     public class sqliteInt : sqliteBase
     {
         public sqliteInt(string Name) : base(Name) { }
-        public int value { get; set; }
+        public new int value { get; set; }
     }
     public class sqliteString : sqliteBase
     {
         public sqliteString(string Name) : base(Name) { }
-        public string value { get; set; }
+        public new string value { get; set; }
     }
     public class sqliteDateTime : sqliteBase
     {
         public sqliteDateTime(string Name) : base(Name) { }
-        public DateTime value { get; set; }
+        public new DateTime value { get; set; }
     }
 }
