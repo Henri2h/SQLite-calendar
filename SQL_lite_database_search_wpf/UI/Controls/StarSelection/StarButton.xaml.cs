04e1c0df6f1183e1a1bd579547865dc3a05e44cc
Henri Carnot <henri.carnot@hotmail.fr>
2016-12-19 00:43:17 +0100

    star selection button

---
+++ SQL_lite_database_search_wpf/UI/Controls/StarSelection/StarButton.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SQL_lite_database_search_wpf.UI.Controls.StarSelection
+{
+    /// <summary>
+    /// Interaction logic for StarButton.xaml
+    /// </summary>
+    public partial class StarButton : Button
+    {
+        // this is for the UI
+        public int StarPosition { get; set; }
+
+
+        public bool enabled = false;
+        public enum displayMode
+        {
+            switchMode,
+            alwaysEnabledMode
+        }
+        public displayMode mode = displayMode.alwaysEnabledMode;
+
+        public displayMode modeD { set { mode = value; } get { return mode; } }
+        public StarButton()
+        {
+            InitializeComponent();
+            updateUI();
+        }
+
+        public bool isStarEnabled
+        {
+            set
+            {
+                enabled = value;
+                updateUI();
+            }
+            get { return enabled; }
+        }
+
+
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (mode == displayMode.switchMode) enabled = !enabled;
+
+            updateUI();
+        }
+
+
+        void updateUI()
+        {
+            
+            if (enabled == false) { UIImage.Source = new BitmapImage(new Uri("pack://application:,,,/UI/Controls/StarSelection/appbar.star.disabled.png")); }
+            else { UIImage.Source = new BitmapImage(new Uri("pack://application:,,,/UI/Controls/StarSelection/appbar.star.png")); }
+        }
+    }
+}
---
+++ SQL_lite_database_search_wpf/UI/Controls/StarSelection/StarsSelecter.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SQL_lite_database_search_wpf.UI.Controls.StarSelection
+{
+    /// <summary>
+    /// Interaction logic for StarsSelecter.xaml
+    /// </summary>
+    public partial class StarsSelecter : UserControl
+    {
+
+        public int starPosition = 0;
+
+        public StarsSelecter()
+        {
+            InitializeComponent();
+        }
+
+        private void UIButton_Click(object sender, RoutedEventArgs e)
+        {
+            StarButton sBt = (StarButton)sender;
+
+            starPosition = sBt.StarPosition;
+
+            UIButton0.isStarEnabled = false;
+            UIButton1.isStarEnabled = false;
+            UIButton2.isStarEnabled = false;
+            UIButton3.isStarEnabled = false;
+            UIButton4.isStarEnabled = false;
+
+            if (sBt.StarPosition >= 0) { UIButton0.isStarEnabled = true; }
+            if (sBt.StarPosition >= 1) { UIButton1.isStarEnabled = true; }
+            if (sBt.StarPosition >= 2) { UIButton2.isStarEnabled = true; }
+            if (sBt.StarPosition >= 3) { UIButton3.isStarEnabled = true; }
+            if (sBt.StarPosition == 4) { UIButton4.isStarEnabled = true; }
+        }
+
+    }
+}
