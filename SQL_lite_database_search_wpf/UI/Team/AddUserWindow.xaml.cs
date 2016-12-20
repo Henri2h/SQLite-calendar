e63f9442e8241e08bf3172aeebfc8b6a13801a54
Henri Carnot <henri.carnot@hotmail.fr>
2016-12-20 15:55:12 +0100

    cleaning the UI on the team pages and working in missing database error handeling

--- SQL_lite_database_search_wpf/UI/Team/AddUserWindow.xaml.cs
+++ SQL_lite_database_search_wpf/UI/Team/AddUserWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using SQL_lite_database_search_wpf.Core.DatabaseItems;
+﻿using FirstFloor.ModernUI.Windows.Controls;
+using SQL_lite_database_search_wpf.Core.DatabaseItems;
 using System.Windows;
 
 namespace SQL_lite_database_search_wpf.UI.Team
@@ -6,7 +7,7 @@ namespace SQL_lite_database_search_wpf.UI.Team
     /// <summary>
     /// Interaction logic for AddUserWindow.xaml
     /// </summary>
-    public partial class AddUserWindow : Window
+    public partial class AddUserWindow : ModernWindow
     {
         public AddUserWindow()
         {
@@ -18,6 +19,12 @@ namespace SQL_lite_database_search_wpf.UI.Team
             EquipeMember eMember = new EquipeMember();
             eMember.name.value = UITbUserName.Text;
             Core.AppCore.dCore.EquipeMemberManager.addMember(eMember);
+            this.DialogResult = true;
+        }
+
+        private void UIBtKo_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = true;
         }
     }
 }
--- SQL_lite_database_search_wpf/UI/Team/TeamView.xaml.cs
+++ SQL_lite_database_search_wpf/UI/Team/TeamView.xaml.cs
@@ -1,18 +1,9 @@
 ﻿using SQL_lite_database_search_wpf.Core.DatabaseItems;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace SQL_lite_database_search_wpf.UI.Team
 {
@@ -24,23 +15,62 @@ namespace SQL_lite_database_search_wpf.UI.Team
         public TeamView()
         {
             InitializeComponent();
+            loadTeam();
         }
         void loadTeam()
         {
-            UIStackTeamElements.Children.Clear();
-            List<EquipeMember> equipeMembers = Core.AppCore.dCore.EquipeMemberManager.getEquipeMembers();
-
-            foreach (EquipeMember eMemb in equipeMembers)
+            try
             {
-                PersonView pView = new PersonView(eMemb);
-                UIStackTeamElements.Children.Add(pView);
+                UIStackTeamElements.Children.Clear();
+                List<EquipeMember> equipeMembers = Core.AppCore.dCore.EquipeMemberManager.getEquipeMembers();
 
+                foreach (EquipeMember eMemb in equipeMembers)
+                {
+                    PersonView pView = new PersonView(eMemb);
+                    pView.HorizontalAlignment = HorizontalAlignment.Left;
+                    pView.VerticalAlignment = VerticalAlignment.Top;
+
+                    UIStackTeamElements.Children.Add(pView);
+
+                }
             }
+            catch (Exception ex)
+            {
+                ex.Source = "SQL_lite_database_search_wpf.UI.Team.TeamView.loadTeam()";
+                ErrorHandeler.ErrorMessage.logError(ex);
+
+
+                UIStackTeamElements.Children.Clear();
+
+                TextBlock tb = new TextBlock();
+                tb.Text = "no team table";
+                UIStackTeamElements.Background = new SolidColorBrush(Colors.Red);
+
+                Button btAddTable = new Button();
+                btAddTable.Content = "Create new  Team table";
+                btAddTable.Click += AddTableClick;
+                btAddTable.HorizontalAlignment = HorizontalAlignment.Left;
+
+
+                UIStackTeamElements.Children.Add(tb);
+                UIStackTeamElements.Children.Add(btAddTable);
+            }
+        }
+
+        private void AddTableClick(object sender, RoutedEventArgs e)
+        {
+            Core.AppCore.dCore.EquipeMemberManager.createEquipeTable();
+            loadTeam();
+            UIStackTeamElements.Background = new SolidColorBrush(Colors.White);
         }
 
         private void UIBtAddUser_Click(object sender, RoutedEventArgs e)
         {
-            
+            AddUserWindow inputDialog = new AddUserWindow();
+            inputDialog.Style = (Style)App.Current.Resources["BlankWindow"];
+            inputDialog.ShowDialog();
+
+            loadTeam();
         }
     }
 }
