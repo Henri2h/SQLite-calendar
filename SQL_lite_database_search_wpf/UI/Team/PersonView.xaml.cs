c23f7d1aa23744fddd9c088defa156211c04f7a3
Henri Carnot <henri.carnot@hotmail.fr>
2016-12-20 16:05:27 +0100

    added a way to delete the user

--- SQL_lite_database_search_wpf/UI/Team/PersonView.xaml.cs
+++ SQL_lite_database_search_wpf/UI/Team/PersonView.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using SQL_lite_database_search_wpf.Core.DatabaseItems;
+using System;
+using System.Windows;
 using System.Windows.Controls;
 
 namespace SQL_lite_database_search_wpf.UI.Team
@@ -8,11 +10,33 @@ namespace SQL_lite_database_search_wpf.UI.Team
     /// </summary>
     public partial class PersonView : UserControl
     {
+        EquipeMember equipeMember { get; set; }
         public PersonView(EquipeMember eMemb)
         {
             InitializeComponent();
-            UITbName.Text = eMemb.name.value;
+            equipeMember = eMemb;
+        }
 
+        void loadElement()
+        {
+            UITbName.Text = equipeMember.name.value;
+        }
+
+        private void UIBtDeleteUser_Click(object sender, System.Windows.RoutedEventArgs e)
+        {
+            try
+            {
+                Core.AppCore.dCore.EquipeMemberManager.deleteEquipeMember(equipeMember);
+                this.Visibility = System.Windows.Visibility.Collapsed;
+            }
+            catch (Exception ex)
+            {
+                ex.Source = " SQL_lite_database_search_wpf.UI.Team.PersonView.UIBtDeleteUser_Click";
+                ErrorHandeler.ErrorMessage.logError(ex);
+                MessageBox.Show("Could not delete the user");
+            }
+
+            
         }
     }
 }
