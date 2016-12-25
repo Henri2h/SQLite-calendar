212fa6f690ebd1a7a47f65855d7b5bcae687f1fb
Henri2h <henri.carnot@hotmail.fr>
2016-12-25 23:49:26 +0100

    added color

--- SQL_lite_database_search_wpf/Core/DatabaseItems/objects/sqliteObjects.cs
+++ SQL_lite_database_search_wpf/Core/DatabaseItems/objects/sqliteObjects.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Media;
 
 namespace SQL_lite_database_search_wpf.Core.DatabaseItems.objects
 {
@@ -27,13 +28,7 @@ namespace SQL_lite_database_search_wpf.Core.DatabaseItems.objects
             this.dataType = "INTEGER";
         }
         public sqliteInt(string Name, string dataType) : base(Name, dataType) { }
-
-
-
-        public int value { get {return (int)base.baseValue; } set { base.baseValue = value; } }
-
-
-
+        public int value { get { return (int)base.baseValue; } set { base.baseValue = value; } }
     }
 
     // ============== string ===============
@@ -59,5 +54,14 @@ namespace SQL_lite_database_search_wpf.Core.DatabaseItems.objects
         public DateTime value { get { return (DateTime)base.baseValue; } set { base.baseValue = value; } }
 
 
+    }
+
+
+
+    // ============== dateTime ===============
+    public class sqliteColor : sqliteBase
+    {
+        public sqliteColor(string Name) : base(Name) { this.dataType = "TEXT"; }
+        public Color value { get { return (Color)base.baseValue; } set { base.baseValue = value; } }
     }
 }
