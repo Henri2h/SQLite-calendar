feb0de2e2878169f52eb5e8ba4520329bbba7501
Henri2h <henri.carnot@hotmail.fr>
2016-12-30 22:49:06 +0100

    added DatabaseItem interface
    provide base element to allow a standart treatment of database elements

---
+++ SQL_lite_database_search_wpf/Core/DatabaseItems/DatabaseItem.cs
@@ -0,0 +1,15 @@
+﻿using SQL_lite_database_search_wpf.Core.DatabaseItems.objects;
+
+namespace SQL_lite_database_search_wpf.Core.DatabaseItems
+{
+    public interface DatabaseItem
+    {
+        sqliteBase[] values { get; set; }
+        void createCalendarObject();
+
+        // needed type
+        sqliteInt elementID { get; set; }
+
+        string tableName { get; set; }
+    }
+}
