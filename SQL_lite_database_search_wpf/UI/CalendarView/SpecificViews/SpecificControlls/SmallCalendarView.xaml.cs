4dfd7630239a109adeaafd390be73b936f985aba
Henri2h <henri.carnot@hotmail.fr>
2017-03-26 17:00:34 +0200

    controlls used by calendar view

---
+++ SQL_lite_database_search_wpf/UI/CalendarView/SpecificViews/SpecificControlls/SmallCalendarView.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SQL_lite_database_search_wpf.UI.CalendarView.SpecificViews.SpecificControlls
+{
+    /// <summary>
+    /// Interaction logic for SmallCalendarView.xaml
+    /// </summary>
+    public partial class SmallCalendarView : UserControl
+    {
+        calendarObject CalendarObject { get; set; }
+
+        public SmallCalendarView(calendarObject cObj)
+        {
+            InitializeComponent();
+            CalendarObject = cObj;
+
+
+            this.Background = new SolidColorBrush(cObj.color.value);
+            this.UITbName.Text = cObj.name.value;
+
+            if (cObj.description.value != "" && cObj.description.value != null)
+            {
+                this.ToolTip = cObj.description.value;
+            }
+        }
+
+        private void UIMenuChangeColor_Click(object sender, RoutedEventArgs e)
+        {
+            UI.UIObjectManager.changeCalendarObjectColor(CalendarObject);
+        }
+
+        private void UIMenuChangeItem_Click(object sender, RoutedEventArgs e)
+        {
+            UI.UIObjectManager.changeCalendarObject(CalendarObject);
+        }
+    }
+}
---
+++ SQL_lite_database_search_wpf/UI/CalendarView/SpecificViews/SpecificControlls/WeekView.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SQL_lite_database_search_wpf.UI.CalendarView.SpecificViews.SpecificControlls
+{
+    /// <summary>
+    /// Interaction logic for WeekView.xaml
+    /// </summary>
+    public partial class WeekCalendarView : UserControl
+    {
+        private DayElement[] dayElement;
+
+
+        public WeekCalendarView(DayElement[] elements)
+        {
+            InitializeComponent();
+
+            if (elements.Length == 7)
+                for (int i = 0; i < 7; i++)
+                {
+                    addChildren(elements[i], i);
+                }
+        }
+        void addChildren(DayElement d, int pos)
+        {
+            DayElementView dayElement = new DayElementView(d);
+            UIGrid.Children.Add(dayElement);
+            Grid.SetColumn(dayElement, pos);
+        }
+    }
+}
