309ad6fa56fe2873381ea8f1e41a596a61559ef7
Henri2h <henri.carnot@hotmail.fr>
2017-04-13 09:27:06 +0200

    added new UI settings
    This class is here to centralise the colors settings of the app

---
+++ SQL_lite_database_search_wpf/UI/UISettings.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace SQL_lite_database_search_wpf.UI
+{
+    public class UISettings
+    {
+        public static Color isSameDayColor = Colors.LightBlue;
+        public static Color isDifferentMontColor = Colors.LightGray;
+        public static Color defaultColor = Colors.White;
+        internal static Color defaultBorderColor = Colors.DarkGray;
+    }
+}
